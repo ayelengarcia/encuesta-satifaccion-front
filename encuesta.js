c18282258bd95b3a7a0fdc557070111a4b183e2a
Ayelen Garcia <ayelengarcia7@gmail.com>
2023-09-18 13:41:58 -0300

    first commit

---
+++ encuesta.js
@@ -0,0 +1,166 @@
+const PRODUCTOS = [];
+const URLproductos = "http://127.0.0.1:8080/todo/productos";
+const URLlista = "http://127.0.0.1:8080/todo";
+const encuestaForm = document.getElementById("encuestaForm");
+const resultado = document.getElementById("resultado");
+const enviarBtn = document.getElementById("enviarBtn");
+const secciones = document.querySelectorAll(".seccion");
+const error = document.getElementById("error");
+resultado.classList.add("d-none");
+
+fetch(URLproductos)
+  .then((response) => response.json())
+  .then((data) => {
+    PRODUCTOS.push(...data);
+
+    encuestaForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      // Obtener selecciones del user
+      const tipo = obtenerSeleccion("tipo");
+      const genero = obtenerSeleccion("genero");
+      const incontinencia = obtenerSeleccion("incontinencia");
+      const talle = obtenerSeleccion("talle");
+
+      // Obtener datos personales del user
+      const nombre = document.getElementById("nombre").value;
+      const apellido = document.getElementById("apellido").value;
+      const celular = document.getElementById("celular").value;
+      const email = document.getElementById("email").value;
+
+      // Buscar producto basado en las seleccion del user
+      const productoFiltrado = filtrarProductos(
+        tipo,
+        genero,
+        incontinencia,
+        talle
+      );
+
+      // Crear objeto con los datos del user
+      const respuestaUser = {
+        nombre,
+        apellido,
+        celular,
+        email,
+        tipo: tipo[0],
+        genero: genero[0],
+        incontinencia: incontinencia[0],
+        talle: talle[0],
+      };
+
+      // Mapear productos filtrados y obtener título y EAN
+      const productoSeleccionado = [];
+      productoFiltrado.forEach((producto) => {
+        productoSeleccionado.push({
+          titulo: producto.titulo,
+          EAN: producto.EAN,
+        });
+      });
+
+      //Objeto para lista
+      const datosCompletos = {
+        respuesta: respuestaUser,
+        producto: productoSeleccionado,
+      };
+
+      //Enviar datos a /todo
+      fetch(URLlista, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datosCompletos),
+      })
+        .then((response) => response.json())
+
+        .catch((error) => {
+          console.error("Error al enviar datos al servidor:", error);
+        });
+
+      encuestaForm.classList.add("d-none");
+      resultado.classList.remove("d-none");
+
+      // Mostrar el producto recomendado
+      mostrarProductoRecomendado(productoFiltrado);
+    });
+  });
+
+// Funcion condicional para envio del form
+enviarBtn.addEventListener("click", function (e) {
+  const checkboxesSeleccionadas = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  );
+  if (checkboxesSeleccionadas.length === 4) return;
+  e.preventDefault();
+  error.innerHTML = "Por favor, completa todos los campos.";
+});
+
+//Función para seleccionar un check por seccion
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    const seccion = this.getAttribute("data-seccion");
+
+    checkboxes.forEach((otherCheckbox) => {
+      if (
+        otherCheckbox.getAttribute("data-seccion") === seccion &&
+        otherCheckbox !== this
+      ) {
+        otherCheckbox.checked = false;
+      }
+    });
+  });
+});
+
+// Función para obtener las selecciones del usuario en una sección
+function obtenerSeleccion(seccion) {
+  const checkboxes = document.querySelectorAll(
+    `input[name=${seccion}]:checked`
+  );
+  const seleccion = Array.from(checkboxes).map((checkbox) => checkbox.value);
+
+  return seleccion;
+}
+
+// Función para filtrar productos basados en las seleccion del user
+function filtrarProductos(tipo, genero, incontinencia, talle) {
+  const productosFiltrados = [];
+
+  PRODUCTOS.forEach((producto) => {
+    const cumpleTipo = tipo.some((selTipo) => producto.tipo.includes(selTipo));
+    const cumpleTalle = talle.some((selTalle) =>
+      producto.talle.includes(selTalle)
+    );
+    const cumpleGenero = genero.some((selGenero) =>
+      producto.genero.includes(selGenero)
+    );
+    const cumpleIncontinencia = incontinencia.some((selIncontinencia) =>
+      producto.incontinencia.includes(selIncontinencia)
+    );
+
+    if (cumpleTipo && cumpleGenero && cumpleIncontinencia && cumpleTalle) {
+      productosFiltrados.push(producto);
+    }
+  });
+
+  return productosFiltrados;
+}
+
+// Función para mostrar el producto recomendado
+function mostrarProductoRecomendado(productosFiltrados) {
+  if (productosFiltrados.length > 0) {
+    const productoRecomendado = productosFiltrados[0];
+    const resultadoDiv = document.getElementById("productoRecomendado");
+    resultadoDiv.innerHTML = `
+            <h3>${productoRecomendado.titulo}</h3>
+            <p>${productoRecomendado.descripción}</p>
+            <img src="${productoRecomendado.imagen}" alt="${productoRecomendado.titulo}">
+            <p>Marca: ${productoRecomendado.marca}</p>
+            <p>EAN: ${productoRecomendado.EAN}</p>
+        `;
+  } else {
+    document.getElementById("productoRecomendado").innerHTML =
+      "<p>No se encontraron productos que coincidan con tus preferencias. Selecciona todas las preferencias.</p>";
+  }
+}
---
+++ lista.js
@@ -0,0 +1,82 @@
+// Conecto con API
+fetch("http://127.0.0.1:8080/todo")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    const datos = data.map((item) => ({
+      ID: item.id,
+      Nombre: item.respuesta.nombre,
+      Apellido: item.respuesta.apellido,
+      Email: item.respuesta.email,
+      Celular: item.respuesta.celular,
+      Titulo: item.producto[0].titulo,
+      EAN: item.producto[0].EAN,
+      Tipo: item.respuesta.tipo,
+      Genero: item.respuesta.genero,
+      Incontinencia: item.respuesta.incontinencia,
+      Talle: item.respuesta.talle,
+    }));
+
+    mostrarDatosEnPagina(data);
+    descargarCSV(datos);
+  })
+  .catch((error) => {
+    console.error("Error al obtener datos del servidor:", error);
+  });
+
+// Función para mostrar los datos en la tabla
+function mostrarDatosEnPagina(datos) {
+  const tablaBody = document.getElementById("tablaBody");
+
+  datos.forEach((dato) => {
+    const filaHTML = `
+  <tr class="columna">
+    <td class="filaP">${dato.id}</td>
+    <td>${dato.respuesta.nombre}</td>
+    <td>${dato.respuesta.apellido}</td>
+    <td class="filaG">${dato.respuesta.email}</td>
+    <td>${dato.respuesta.celular}</td>
+    <td class="filaG">${dato.producto[0].titulo}</td>
+    <td>${dato.producto[0].EAN}</td>
+    <td>${dato.respuesta.tipo}</td>
+    <td>${dato.respuesta.genero}</td>
+    <td>${dato.respuesta.incontinencia}</td>
+    <td class="filaP">${dato.respuesta.talle}</td>
+  </tr>
+`;
+
+    // Agregar la fila a la tabla
+    tablaBody.innerHTML += filaHTML;
+  });
+}
+
+// Función para convertir un arreglo de objetos en formato CSV
+function convertirA_CSV(datos) {
+  const encabezados = Object.keys(datos[0]);
+  const filas = [encabezados.join(",")];
+
+  datos.forEach((dato) => {
+    const fila = encabezados.map((campo) => dato[campo]);
+    filas.push(fila.join(","));
+  });
+
+  return filas.join("\n");
+}
+
+// Función para descargar el archivo CSV
+function descargarCSV(data) {
+  const contenidoCSV = convertirA_CSV(data).replace(/,/g, ";");
+
+  const blob = new Blob([contenidoCSV], { type: "text/csv;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "datos.csv";
+  a.textContent = "Descargar CSV";
+
+  const btnDescargar = document.getElementById("descargar");
+  btnDescargar.appendChild(a);
+}
+
+// 7794626011184;
